9c9a73f8b364edddc939bd4dfa6b559d39361653
Zeph <92046611+fZpHr@users.noreply.github.com>
2025-01-07 21:46:13 +0100

    Create cron.js

---
+++ app/api/cron.js
@@ -0,0 +1,16 @@
+import { exec } from 'child_process';
+
+export default function handler(req, res) {
+  exec('npx prisma db pull', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Erreur: ${error.message}`);
+      return res.status(500).json({ error: error.message });
+    }
+    if (stderr) {
+      console.error(`Stderr: ${stderr}`);
+      return res.status(500).json({ error: stderr });
+    }
+    console.log(`Sortie: ${stdout}`);
+    return res.status(200).json({ message: 'Commande exécutée avec succès', output: stdout });
+  });
+}
