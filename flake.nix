76c1543af04a516b5f069c37a6508523ebb60426
Haletran <bpasquier123@gmail.com>
2025-07-26 18:08:23 +0200

    Add: flake for dev

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import (inputs.nixpkgs) { inherit system; });
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs= [
+            pkgs.nodejs_22
+            pkgs.nodePackages.pnpm
+            pkgs.nodePackages.typescript
+            pkgs.nodePackages.typescript-language-server
+          ];
+          shellHook = ''
+                npm install
+                echo "42INSIGHT DEV-SHELL"
+          '';
+        };
+      }
+    );
+}
